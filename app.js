7d77eb2d91a534b76a10ba62b8d0522729faa5ea
lucas <lucasmio98@gmail.com>
2025-05-23 00:30:48 -0300

    adding a router and serving files with static express only

--- app.js
+++ app.js
@@ -2,77 +2,21 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 const bp = require('body-parser')
+const usersRouter = require("./router/users.routes")
 
-const users = {1:{name:"Lucas",age:24},2:{name:"Pedro",age:22}}
 
 app.use(bp.json())
-app.use(express.static('public'));
 
-app.get("/",(req,res)=>{
-  res.send("Hola desde express")
-})
+//Al poner http://localhost:3000/ ya me despliega, sin necesidad de un app.get en "/"
+app.use(express.static("public"))
+app.use(express.static("public/views"))
+
 
 app.get("/contacto",(req,res)=>{
   res.send("contacto")
 })
 
-app.get("/api/users",(req,res)=>{
-  res.json(users)
-})
-
-app.get("/api/users/:id",(req,res)=>{
-  const { id } = req.params
-  res.send(users[id])
-})
-
-app.post("/api/users",(req,res)=>{
-  const datos = req.body;
-  console.log(datos)
-  if ("id" in datos && "name" in datos && "age" in datos && !(datos["id"] in users)){
-    users[datos["id"]]={name:datos["name"],age:datos["age"]}
-
-    res.json(users)
-  } else {
-    console.log("id" in datos)
-    console.log("name" in datos)
-    console.log("age" in datos)
-    console.log(!(datos["id"] in users))
-    console.log("id" in datos && "name" in datos && "age" in datos && !datos["id"] in users)
-    res.send("Ocurrio un error al mandar el dato")
-  }
-
-})
-
-app.put("/api/users/:id",(req,res)=>{
-  const { id } = req.params
-  const datos = req.body;
-  console.log(datos)
-  if (!(id in users)){
-    res.send(`No existe el id ${id} en ${JSON.stringify(users)}`)
-    return
-  }
-  if (!("age" in datos && "name" in datos)){
-    res.send(`No existe el campo age o name en ${JSON.stringify(datos)}`)
-    return
-  }
-  users[id]["name"]=datos["name"]
-  users[id]["age"]=datos["age"]
-  res.send(users)
-})
-
-app.delete("/api/users/:id",(req,res)=>{
-  const { id } = req.params
-  if (!(id in users)){
-    res.send(`No existe el id ${id} en ${JSON.stringify(users)}`)
-    return
-  }
-  delete users[id]
-  res.send(users)
-})
-
-app.get("/mipagina",(req,res)=>{
-  res.sendFile(__dirname+"/public/views/index.html")
-})
+app.use("/api/users",usersRouter)
 
 app.listen(PORT,()=>{
   console.log(`Server working in http://localhost:3000`)
