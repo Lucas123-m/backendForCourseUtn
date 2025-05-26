23391de2cb69ced889f5ac461e011bf92973c2ea
lucas <lucasmio98@gmail.com>
2025-05-25 22:00:32 -0300

    adding ejs

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ const app = express()
 const PORT = 3000
 const bp = require('body-parser')
 const usersRouter = require("./router/users.routes")
+const path = require("path")
+require("ejs")
+
 const morgan = require("morgan")
+app.set("views",path.join(__dirname,"public","views"))
+app.set("view engine","ejs")
 
 //middleware logger
 app.use(morgan())
-/*app.use((req,res, next)=>{
-  //se ejecuta al ingresar cualqueir url.
-  //es un middleware de tipo logger
-  console.log(`Ruta ${req.url}, metodo ${req.method}`)
-  next()
-})*/
 app.use(bp.json())
 
 //Al poner http://localhost:3000/ ya me despliega, sin necesidad de un app.get en "/"
 app.use(express.static("public"))
 app.use(express.static("public/views"))
 
-
 app.get("/contacto",(req,res)=>{
   res.send("contacto")
+})
+
+app.get("/ejs",(req,res)=>{
+  const title  = "xdddd"
+  res.render("page",{title:title})  
 })
 
 app.use("/api/users",usersRouter)
