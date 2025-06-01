71f058264ef15c28605b126742dce23ff649d637
lucas <lucasmio98@gmail.com>
2025-06-01 19:42:11 -0300

    Revert "mail sending config"
    This reverts commit c50d23202b0f62682848bbe221996eb261e80e90.

--- router/ejs.routes.js
+++ router/ejs.routes.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const router = express.Router()
 const axios = require("axios")
-const mailController = require('./../controller/mail.controller');
 
 router.get("/",(req,res)=>{
     const titleContent  = "Hola a todos desde ejs"
@@ -30,10 +29,4 @@ router.get("/animes",async (req,res)=>{
     const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
     res.render("ejs/dashboard",{posts:response.data})
 })
-
-router.get("/contact",(req,res)=>{
-    res.render("ejs/contact")
-})
-router.post("/send-email",mailController.sendEmail)
-
 module.exports = router;
