59b9d6d3d76e6e85fa0e1672a88a682c38364506
lucas <lucasmio98@gmail.com>
2025-06-24 23:42:52 -0300

    import cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const animeRouter = require("./src/routes/anime.routes")
 
 const app = express();
 
 app.use(express.json())
-
+app.use(cors)
 app.use("/animes",animeRouter)
 
 const PORT = process.env.PORT || 3000;
