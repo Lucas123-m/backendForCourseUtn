4f7b99010596f938a86d14b80e0c88e361fdac27
lucas <lucasmio98@gmail.com>
2025-06-29 19:23:42 -0300

    minor changes in services file

--- src/services/anime.service.js
+++ src/services/anime.service.js
@@ -21,8 +21,9 @@ exports.getAllContent = async () => {
     const [rows] = await pool.query(`
         SELECT
         a.id,
+        a.id_serie,
         b.title as title_serie,
-        a.title as title_cotent,
+        a.title as title_content,
         a.type,
         a.watch_order,
         a.chapters,
